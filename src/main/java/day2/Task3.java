f43a77da0e632c28c040dec84a7d368c951f3d7d
vdjdf <vovagost1@yandex.ru>
2022-05-05 19:14:17 +0300

    day2

--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a >= b) {
+            System.out.print("Некорекнтый ввод");
+        } else {
+
+            while (a < b - 1) {
+                ++a;
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.print(a + " ");
+                }
+            }
+        }
     }
 }
--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int x = scanner.nextInt();
+        if (x >= 5) {
+            System.out.println((x * x - 10.0) / (x + 7.0));
+        } else if (-3 < x && x < 5) {
+            System.out.println((x + 3.0) * (x * x - 2.0));
+        } else {
+            System.out.println("420.0");
+        }
 
     }
 }

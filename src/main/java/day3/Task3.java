012188b34da2549bd8202000becb9d62f4dc1cbe
vdjdf <VovaGost1@yandex.ru>
2022-05-06 14:37:45 +0500

    day3

--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 public class Task3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        while (true){
+int i = 0 ;
+        for(i = 0; i<5;i++){
             double a = scanner.nextDouble();
             double b = scanner.nextDouble();
             if (b == 0 ) {
-                System.out.print("Деление на 0");
+                System.out.print("Деление на 0"+ "\n");
+                --i;
                 continue;
             }
             System.out.print(a / b + "\n");
